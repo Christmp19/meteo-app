fba6bbfe3c1dac5c82a8f325c4da7a031cfa92db
Chritie MPASSI <christmp19@gmail.com>
2024-06-11 09:41:02 +0100

    feat: initialize all dependencies

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import { View, Text } from "react-native";
+import React from "react";
+import About from "./components/About";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <About />
+    );
+  }
 }
---
+++ components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default class About extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>A propos de moi</Text>
+                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque a laboriosam culpa facere, excepturi, ab voluptatum eius laborum dolores perferendis tempora aliquam quis accusantium ipsum quaerat. Itaque iure officiis expedita!</Text>
+            </View>
+        )
+    }
+}
